c533e3567cdc90244cd6c896a338019dc72a312a
Madhan M <themaive.in@gmail.com>
2025-07-02 19:19:21 +0530

    Wind up back to original

--- bot.py
+++ bot.py
@@ -2,10 +2,6 @@ import dotenv
 from telegram.ext import Application, CommandHandler, ContextTypes, PrefixHandler
 from telegram import Update
 
-from utils.logger import logger
-
-from handlers.encrypt_handler import EncryptHandler
-from handlers.start_handler import StartHandler
 
 TOKEN = dotenv.get_key(key_to_get='TOKEN', dotenv_path='.env', encoding='utf-8')
 
@@ -13,16 +9,5 @@ TOKEN = dotenv.get_key(key_to_get='TOKEN', dotenv_path='.env', encoding='utf-8')
 if __name__ == '__main__':
 
     application = Application.builder().token(TOKEN).build()
-    logger.log(level=0, msg="Application started")
-
-    start_handler = StartHandler()
-    encrypt_handler = EncryptHandler(application)
-
-    application.add_handler(CommandHandler("start", start_handler.start))
-    application.add_handler(CommandHandler("description", start_handler.description))
-
-
-    application.add_handler(PrefixHandler("?", "encrypt_aes", encrypt_handler.handle_aes_encrypt))
-
 
     application.run_polling()
--- utils/logger.py
+++
@@ -1,9 +0,0 @@
-import logging
-
-logging.basicConfig(
-    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
-)
-
-logging.getLogger('httpx').setLevel(logging.WARNING)
-
-logger = logging.getLogger(__name__)
